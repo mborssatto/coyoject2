6e9da3f8faa62e83645a5678d5e00b928c21fb83
mborssatto <mborssatto@gmail.com>
2022-11-24 10:33:01 +0000

    Learning express.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,20 +4,31 @@ const { users } = require('../models')
 const router = express.Router()
 
 /* GET users listing. */
-router.get('/', (req, res) => {
-  res.send('respond with a resource')
+router.get('/', function (req, res, next) {
+  let result = users
+
+  if (req.query.name) {
+    result = users.find(user => user.name === req.query.name)
+  }
+  res.send(result)
 })
 
-router.get('/:name', (req, res) => {
-  const user = users.find(u => u.name === req.params.name)
-
-  if (!user)
-    return res.render('error', {
-      error: { status: 404 },
-      message: `No user with name ${req.params.name} found`,
-    })
-
-  return user
+router.get('/:userID', function (req, res, next) {
+  const user = users[req.params.userID]
+  if (user) res.send(user)
+  else res.sendStatus(404)
 })
+
+// router.get('/:name', (req, res) => {
+//   const user = users.find(u => u.name === req.params.name)
+
+//   if (!user)
+//     return res.render('error', {
+//       error: { status: 404 },
+//       message: `No user with name ${req.params.name} found`,
+//     })
+
+//   return user
+// })
 
 module.exports = router
