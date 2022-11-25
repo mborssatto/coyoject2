fbed35e8c174c28e2d14b028798e8a4e233ad5b6
mborssatto <mborssatto@gmail.com>
2022-11-25 13:02:50 +0000

    add render event detail page

--- src/routes/events.js
+++ src/routes/events.js
@@ -6,14 +6,26 @@ const router = express.Router()
 /* GET events listing 
 and FILTER by CITY - WORKS with url http://localhost:3000/events?city=Berlin */
 
-router.get('/', function (req, res, next) {
+router.get('/', (req, res) => {
   let result = events
 
   if (req.query.city) {
     result = events.filter(event => event.city === req.query.city)
   }
 
-  res.render('events', { result })
+  return res.render('events', { result })
+})
+
+router.get('/:id', (req, res) => {
+  const event = events[req.params.id]
+
+  if (!event)
+    return res.render('error', {
+      error: { status: 404 },
+      message: `No event with id ${req.params.id} found`,
+    })
+
+  return res.render('detail', { event })
 })
 
 module.exports = router
